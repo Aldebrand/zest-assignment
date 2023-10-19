8a5cc5e18c4169df160af30f8397d1b6e2411c5f
Or Chen <orchen@viber.com>
2023-10-20 00:46:35 +0300

    add config to the app

--- github_data_service/app/run.py
+++ github_data_service/app/run.py
@@ -2,6 +2,7 @@ from flask import Flask
 from .routes import github_routes
 
 app = Flask(__name__)
+app.config.from_pyfile('config.py')
 app.register_blueprint(github_routes, url_perfix='/github')
 
 if __name__ == '__main__':
