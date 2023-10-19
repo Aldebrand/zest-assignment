30da1a264ab2ae4dcc8f8cb78ac07876d97db804
Or Chen <orchen@viber.com>
2023-10-19 20:12:47 +0300

    add the service config

---
+++ github-data-service/app/config.py
@@ -0,0 +1,7 @@
+GITHUB_API_BASE_URL = 'https://api.github.com'
+GITHUB_API_SEARCH_REPOS_URL = GITHUB_API_BASE_URL + '/search/repositories'
+
+#Redis
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+CACHE_EX = 60*60*24
