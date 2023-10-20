cf55f067fdf3e28666b94206584c082630255abd
Or Chen <orchen@viber.com>
2023-10-20 22:02:01 +0300

    add the db configuration - username and password

--- auth_service/app/config.py
+++ auth_service/app/config.py
@@ -1,3 +1,10 @@
-DB_URI = 'mongodb://mongodb:27017/'
+import os 
+
+DB_USERNAME = os.environ.get('MONGO_USERNAME')
+DB_PASSWORD = os.environ.get('MONGO_PASSWORD')
+DB_HOST = 'mongodb'
+DB_PORT = '27017'
+DB_URI = f'mongodb://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/'
+
 DB_NAME = 'ZEST_DB'
 USERS_COLLECTION = 'users'
