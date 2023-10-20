007ec40036d260068e51a4b420d4adcac5d63deb
Or Chen <orchen@viber.com>
2023-10-20 11:55:51 +0300

    get the host and port from env variable

--- github_data_service/app/run.py
+++ github_data_service/app/run.py
@@ -1,9 +1,13 @@
+import os
 from flask import Flask
-from .routes import github_routes
+from routes import github_routes
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 app.register_blueprint(github_routes, url_perfix='/github')
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    host = os.environ.get('HOST', '0.0.0.0')
+    port = os.environ.get('PORT', 8080)
+    app.run(host=host, port=port, debug=True)
