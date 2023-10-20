07d397b686ab4a458704e4d30aa89b50a9f56e18
Or Chen <orchen@viber.com>
2023-10-20 13:44:01 +0300

    fix type

--- github_data_service/app/run.py
+++ github_data_service/app/run.py
@@ -4,7 +4,7 @@ from routes import github_routes
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
-app.register_blueprint(github_routes, url_perfix='/github')
+app.register_blueprint(github_routes, url_prefix='/github')
 
 
 if __name__ == '__main__':
