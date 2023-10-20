deeb2841202bd4519bb559ea54b9d23f8c56949e
Or Chen <orchen@viber.com>
2023-10-20 12:05:22 +0300

    changed the name of the redishost to fit the current setup

--- github_data_service/app/config.py
+++ github_data_service/app/config.py
@@ -2,6 +2,6 @@ GITHUB_API_BASE_URL = 'https://api.github.com'
 GITHUB_API_SEARCH_REPOS_URL = GITHUB_API_BASE_URL + '/search/repositories'
 
 # Redis
-REDIS_HOST = 'localhost'
+REDIS_HOST = 'redis'
 REDIS_PORT = 6379
 CACHE_EX = 60*60*24
