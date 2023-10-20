6b702c6f377d616ddd3a9042435890b9b2ebdad0
Or Chen <orchen@viber.com>
2023-10-20 19:20:09 +0300

    create the flask app for the auth service

---
+++ auth_service/app/run.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask
+from routes import auth_routes
+
+app = Flask(__name__)
+app.config.from_pyfile('config.py')
+app.register_blueprint(auth_routes, url_prefix='/auth')
+
+
+if __name__ == '__main__':
+    host = os.environ.get('HOST', '0.0.0.0')
+    port = os.environ.get('PORT', 8080)
+    app.run(host=host, port=port, debug=True)
