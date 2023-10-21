6649523a1f5487b2e66e9121149766322f56279b
Or Chen <orchen@viber.com>
2023-10-21 15:27:43 +0300

    implemented the config of the favorite service

---
+++ user_favorites_service/app/config.py
@@ -0,0 +1,11 @@
+import os 
+
+DB_USERNAME = os.environ.get('MONGO_USERNAME')
+DB_PASSWORD = os.environ.get('MONGO_PASSWORD')
+DB_HOST = 'mongodb'
+DB_PORT = '27017'
+DB_URI = f'mongodb://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/'
+
+DB_NAME = 'ZEST_DB'
+FAVORITES_COLLECTION = 'favorites'
+JWT_ALGORITHM = 'HS256'
