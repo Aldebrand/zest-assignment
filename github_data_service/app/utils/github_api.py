9c37c1da17ba762a7fa5af68423d0092c108237a
Or Chen <orchen@viber.com>
2023-10-21 11:39:28 +0300

    added more support for pagination

--- github_data_service/app/utils/github_api.py
+++ github_data_service/app/utils/github_api.py
@@ -8,118 +8,138 @@ from utils.redis_manager import create_redis_client, get_from_cache, set_in_cach
     
 def _get_repositories_from_github(session, sort_by, order, per_page):
     """
-    Retrieves a list of repositories from the GitHub API, sorted by the given criteria.
+    Retrieve a list of up to 100 GitHub repositories sorted by the given criteria.
 
     Args:
-        session (requests.Session): A requests session object.
-        sort_by (str): The field to sort the repositories by.
-        order (str): The order to sort the repositories in (asc or desc).
+        session (requests.Session): A session object to use for making HTTP requests.
+        sort_by (str): The field to sort the repositories by. Valid values are 'stars', 'forks', and 'updated'.
+        order (str): The order to sort the repositories in. Valid values are 'asc' and 'desc'.
         per_page (int): The number of repositories to retrieve per page.
 
     Returns:
-        list: A list of dictionaries, each representing a repository. Each dictionary contains the following keys:
+        list: A list of up to 100 repositories, represented as dictionaries with the following keys:
             - id (int): The ID of the repository.
             - name (str): The name of the repository.
             - description (str): The description of the repository.
-            - owner (dict): A dictionary containing the following keys:
-                - id (int): The ID of the repository owner.
-                - login (str): The username of the repository owner.
-                - avatar_url (str): The URL of the repository owner's avatar.
-                - html_url (str): The URL of the repository owner's GitHub profile.
+            - owner (dict): A dictionary representing the owner of the repository, with the following keys:
+                - id (int): The ID of the owner.
+                - login (str): The username of the owner.
+                - avatar_url (str): The URL of the owner's avatar image.
+                - html_url (str): The URL of the owner's GitHub profile.
             - html_url (str): The URL of the repository on GitHub.
-            - clone_url (str): The URL to clone the repository.
+            - clone_url (str): The URL to use for cloning the repository.
             - language (str): The primary language of the repository.
             - topics (list): A list of topics associated with the repository.
             - stars (int): The number of stars the repository has.
             - forks_count (int): The number of forks the repository has.
-            - created_at (str): The date and time the repository was created.
-            - updated_at (str): The date and time the repository was last updated.
-            - pushed_at (str): The date and time of the last push to the repository.
-            - archived (bool): Whether or not the repository is archived.
-            - visibility (str): The visibility of the repository (public or private).
+            - created_at (str): The date and time the repository was created, in ISO 8601 format.
+            - updated_at (str): The date and time the repository was last updated, in ISO 8601 format.
+            - pushed_at (str): The date and time of the last push to the repository, in ISO 8601 format.
+            - archived (bool): Whether the repository is archived.
+            - visibility (str): The visibility of the repository. Valid values are 'public', 'private', and 'internal'.
             - watchers_count (int): The number of users watching the repository.
             - open_issues_count (int): The number of open issues in the repository.
     """
+def _get_repositories_from_github(session, sort_by, order, per_page):
     
+    repositories = []
+    page = 1
     params = {'q': f'stars:>0', 'sort': sort_by,
               'order': order, 'per_page': per_page, 'page': 1}
     
-    try:
-        response = session.get(
-            app.config['GITHUB_API_SEARCH_REPOS_URL'], params=params)
-    except requests.exceptions.Timeout:
-        logger.error(
-            f"Failed to retrieve top 100 repos by stars. Request timed out.")
-        
-        return []
-    except requests.exceptions.RequestException as e:
-        logger.error(
-            f"Failed to retrieve top 100 repos by stars. Error: {e}")
-        
-        return []
-
-    if response.status_code == 200:
-        logger.info(f"Retrieved top 100 repos by stars from GitHub API")
+    while len(repositories) < 100:
+        params['page'] = page
 
         try:
-            data = response.json().get('items', [])
-        except ValueError as e:
+            response = session.get(
+                app.config['GITHUB_API_SEARCH_REPOS_URL'], params=params)
+        except requests.exceptions.Timeout:
             logger.error(
-                f"Failed to retrieve top 100 repos by stars. Error: {e}")
+                f"Time out while retrieving a bulk of repos from GitHub API.")
             
-            return []
+            break
+        except requests.exceptions.RequestException as e:
+            logger.error(
+                f"RequestException: {e} while retrieving a bulk of repos from GitHub API.")
+            
+            break
 
-        repositories = []
-
-        for repo in data:
+        if response.status_code == 200:
             try:
-                owner_data = repo['owner']
-                owner = {
-                    'id': owner_data['id'],
-                    'login': owner_data['login'],
-                    'avatar_url': owner_data['avatar_url'],
-                    'html_url': owner_data['html_url']
-                }
-                repositories.append({
-                    'id': repo['id'],
-                    'name': repo['name'],
-                    'description': repo['description'],
-                    'owner': owner,
-                    'html_url': repo['html_url'],
-                    'clone_url': repo['clone_url'],
-                    'language': repo['language'],
-                    'topics': repo['topics'],
-                    'stars': repo['stargazers_count'],
-                    'forks_count': repo['forks_count'],
-                    'created_at': repo['created_at'],
-                    'updated_at': repo['updated_at'],
-                    'pushed_at': repo['pushed_at'],
-                    'archived': repo['archived'],
-                    'visibility': repo['visibility'],
-                    'watchers_count': repo['watchers_count'],
-                    'open_issues_count': repo['open_issues_count']
-                })
-            except KeyError as e:
+                data = response.json().get('items', [])
+                logger.info(f"Retrieved {len(data)} repos from GitHub API")
+            except ValueError as e:
                 logger.error(
-                    f"KeyError: {e} in repo: {repo}. Failed to retrieve top 100 repos by stars.")
+                    f"Falied to parse response from GitHub API. Error: {e}")
                 
-                return []
+                break
 
-        return repositories
-    
-    elif response.status_code == 403 and 'X-RateLimit-Remaining' in response.headers \
-            and response.headers['X-RateLimit-Remaining'] == '0':
-        logger.error(
-            f"GitHub API rate limit exceeded.")
-    elif  response.status_code == 403 and 'Retry-After' in response.headers:
-        retry_after = response.headers['Retry-After']
-        logger.error(f'GitHub API abuse detection triggered. Retry after {retry_after} seconds.')
-    else:
-        logger.error(
-            f"Failed to retrieve top 100 repos by stars. "
-            f"Status code: {response.status_code}, Response: {response.text}") 
+            for repo in data:
+                try:
+                    owner_data = repo['owner']
+                    owner = {
+                        'id': owner_data['id'],
+                        'login': owner_data['login'],
+                        'avatar_url': owner_data['avatar_url'],
+                        'html_url': owner_data['html_url']
+                    }
+                    repositories.append({
+                        'id': repo['id'],
+                        'name': repo['name'],
+                        'description': repo['description'],
+                        'owner': owner,
+                        'html_url': repo['html_url'],
+                        'clone_url': repo['clone_url'],
+                        'language': repo['language'],
+                        'topics': repo['topics'],
+                        'stars': repo['stargazers_count'],
+                        'forks_count': repo['forks_count'],
+                        'created_at': repo['created_at'],
+                        'updated_at': repo['updated_at'],
+                        'pushed_at': repo['pushed_at'],
+                        'archived': repo['archived'],
+                        'visibility': repo['visibility'],
+                        'watchers_count': repo['watchers_count'],
+                        'open_issues_count': repo['open_issues_count']
+                    })
+                except KeyError as e:
+                    logger.error(
+                        f"KeyError: {e} in repo: {repo}. Stop retrieving repos from GitHub API.")
+                    
+                    break
+        elif response.status_code == 403 and 'X-RateLimit-Remaining' in response.headers \
+                and response.headers['X-RateLimit-Remaining'] == '0':
+            logger.error(
+                f"GitHub API rate limit exceeded.")
+            
+            break
+        elif  response.status_code == 403 and 'Retry-After' in response.headers:
+            retry_after = response.headers['Retry-After']
+            logger.error(f'GitHub API abuse detection triggered. Retry after {retry_after} seconds.')
+
+            break
+        else:
+            logger.error(
+                f"Failed to retrieve top 100 repos by stars. "
+                f"Status code: {response.status_code}, Response: {response.text}")
+            
+            break
+
+        # Check if there is next page. If not, break the loop
+        if 'link' in response.headers:
+            links = response.headers['link'].split(',')
+            for link in links:
+                if 'rel="next"' in link:
+                    
+                    break
+            else:
+                break
+        else:
+            break
+
+        page += 1
         
-    return []
+    return repositories[:100]
 
 
 def create_session(token=None):
