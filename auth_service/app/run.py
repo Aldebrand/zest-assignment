0e97ef1a2e03a911aba0c06cca79370988591696
Or Chen <orchen@viber.com>
2023-10-21 15:22:14 +0300

    replace the defult port

--- auth_service/app/run.py
+++ auth_service/app/run.py
@@ -10,5 +10,5 @@ app.register_blueprint(auth_routes, url_prefix='/auth')
 
 if __name__ == '__main__':
     host = os.environ.get('HOST', '0.0.0.0')
-    port = os.environ.get('PORT', 8080)
+    port = os.environ.get('PORT', 8081)
     app.run(host=host, port=port, debug=True)
