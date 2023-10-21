b60a2914f2a2327e1959d0fe42e1a05a3946d51e
Or Chen <orchen@viber.com>
2023-10-21 15:28:53 +0300

    implemented the flask app of the service

---
+++ user_favorites_service/app/run.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask
+
+from routes import favorite_repos_routes
+
+app = Flask(__name__)
+app.config.from_pyfile('config.py')
+app.register_blueprint(favorite_repos_routes, url_prefix='/favorites')
+
+
+if __name__ == '__main__':
+    host = os.environ.get('HOST', '0.0.0.0')
+    port = os.environ.get('PORT', 8082)
+    app.run(host=host, port=port, debug=True)
